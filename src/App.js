a4bdb61fd8f35b1789d83c34a32635a2a1358cd9
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-13 07:37:03 +1100

    Get base64 string from image url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import imageToBase64 from "image-to-base64/browser";
+
 function App() {
+  const [base64, setBase64] = useState("");
+
+  useEffect(() => {
+    imageToBase64("https://placekitten.com/200/300") // Path to the image
+      .then(response => {
+        console.log(response); // "cGF0aC90by9maWxlLmpwZw=="
+        return setBase64(response);
+      })
+      .catch(error => {
+        console.log(error); // Logs an error if there was one
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <p>{base64}</p>
     </div>
   );
 }
