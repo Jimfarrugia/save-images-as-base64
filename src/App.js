580161690d25e156b0503a0f43ece85ac8f5f981
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-13 07:48:37 +1100

    Save image dataUrl to localStorage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import { useState, useEffect } from "react";
 import imageToBase64 from "image-to-base64/browser";
 
 function App() {
-  const [imageSrc, setImageSrc] = useState("");
-
   useEffect(() => {
     imageToBase64("https://placekitten.com/200/300") // Path to the image
       .then(response => {
         console.log(response); // "cGF0aC90by9maWxlLmpwZw=="
         const prefix = "data:image/jpg;base64,";
-        const base64 = response;
-        return setImageSrc(prefix + base64);
+        const dataUrl = prefix + response;
+        return localStorage.setItem("image", dataUrl);
       })
       .catch(error => {
         console.log(error); // Logs an error if there was one
@@ -20,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <p>
-        <img src={imageSrc} alt="" />
+        <img src={localStorage.getItem("image")} alt="" />
       </p>
     </div>
   );
