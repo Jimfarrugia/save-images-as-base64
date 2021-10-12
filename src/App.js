f480249f4c4f766fc6993c8af597c4bc8eb272e3
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-13 08:19:40 +1100

    Add form for providing image url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useRef } from "react";
 import imageToBase64 from "image-to-base64/browser";
 
-const saveImageToLocalStorage = imageUrl => {
-  imageToBase64(imageUrl) // Path to the image
+const saveImageToLocalStorage = url => {
+  imageToBase64(url)
     .then(base64 => {
       const prefix = "data:image/jpg;base64,";
       const dataUrl = prefix + base64;
@@ -12,12 +12,19 @@ const saveImageToLocalStorage = imageUrl => {
 };
 
 function App() {
-  useEffect(() => {
-    saveImageToLocalStorage("http://placekitten.com/200/300");
-  }, []);
+  const inputRef = useRef(null);
+
+  const handleSubmit = () => {
+    saveImageToLocalStorage(inputRef.current.value);
+  };
 
   return (
     <div className="App">
+      <p>Enter Image URL:</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+        <input type="submit" />
+      </form>
       <p>
         <img src={localStorage.getItem("image")} alt="" />
       </p>
