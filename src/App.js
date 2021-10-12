794b8226d0fbf49b1bd42139baa8f4f8a151aee1
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-13 07:59:59 +1100

    Create helper function for saving img to localStorage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import imageToBase64 from "image-to-base64/browser";
 
+const saveImageToLocalStorage = imageUrl => {
+  imageToBase64(imageUrl) // Path to the image
+    .then(base64 => {
+      const prefix = "data:image/jpg;base64,";
+      const dataUrl = prefix + base64;
+      return localStorage.setItem("image", dataUrl);
+    })
+    .catch(error => console.log(error));
+};
+
 function App() {
   useEffect(() => {
-    imageToBase64("https://placekitten.com/200/300") // Path to the image
-      .then(response => {
-        console.log(response); // "cGF0aC90by9maWxlLmpwZw=="
-        const prefix = "data:image/jpg;base64,";
-        const dataUrl = prefix + response;
-        return localStorage.setItem("image", dataUrl);
-      })
-      .catch(error => {
-        console.log(error); // Logs an error if there was one
-      });
+    saveImageToLocalStorage("http://placekitten.com/200/300");
   }, []);
 
   return (
