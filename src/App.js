73908e5bf6cb5f83d90dfd1f157a205264898db9
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-13 07:44:04 +1100

    Add hardcoded prefix and render image w/ base64 as src

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { useState, useEffect } from "react";
 import imageToBase64 from "image-to-base64/browser";
 
 function App() {
-  const [base64, setBase64] = useState("");
+  const [imageSrc, setImageSrc] = useState("");
 
   useEffect(() => {
     imageToBase64("https://placekitten.com/200/300") // Path to the image
       .then(response => {
         console.log(response); // "cGF0aC90by9maWxlLmpwZw=="
-        return setBase64(response);
+        const prefix = "data:image/jpg;base64,";
+        const base64 = response;
+        return setImageSrc(prefix + base64);
       })
       .catch(error => {
         console.log(error); // Logs an error if there was one
@@ -17,7 +19,9 @@ function App() {
 
   return (
     <div className="App">
-      <p>{base64}</p>
+      <p>
+        <img src={imageSrc} alt="" />
+      </p>
     </div>
   );
 }
